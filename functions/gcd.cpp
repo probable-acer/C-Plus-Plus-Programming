3b74644fb207ea67ef844013a46662d4850151c8
probable-acer <shubhongkar.codes@gmail.com>
2026-07-16 22:17:26 +0530

    correction

--- functions/gcd.cpp
+++ functions/gcd.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 int gcd(int a,int b){
     int hcf = 1;
-    for(int i=min(a,b);i>=1;i--){
+    for(int i=max(a,b);i>=1;i--){
         if(a%i==0 && b%i==0){ // i is a common factor
             hcf = i;
             break;
